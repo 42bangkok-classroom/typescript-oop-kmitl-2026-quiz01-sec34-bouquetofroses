9090ed9d8b692f7e2cc8c91bb2a58a710fdba1e2
bouquetofroses <valenny1402@gmail.com>
2025-12-15 18:11:28 +0700

    up1

--- src/q17/starPyramid.ts
+++ src/q17/starPyramid.ts
@@ -1,6 +1,11 @@
 let size1 = Number(process.argv[2]);
 
 for ( let i = 0 ; i < size1 ; i++ ){
-    console.log("*".repeat(size1));
+    let result4 ="";
+    for (let j=0 ; j<i;j++ ){
+        result4 += "*";
+    }
+    console.log(result4);
+
 }
 
